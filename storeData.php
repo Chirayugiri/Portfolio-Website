0308b4389a3e94cb0a86e314c790227ea9921831
Chirayugiri <chirayugiri149@gmail.com>
2023-11-08 20:51:04 +0530

    fixed database issue

--- storeData.php
+++ storeData.php
@@ -11,24 +11,24 @@
     {   
         echo "Error occurred: ".mysqli_connect_error();
     }
-    else{
-        // echo "connection done<br>";
-    }
 
     //getting form data
     $email=$_POST['email'];
-    $pass=$_POST['pass'];
+    // $pass=$_POST['pass'];
     $msg=$_POST['msg'];
     $date=date("Y-m-d");
 
-    $create="CREATE TABLE IF NOT EXISTS `portfolio`(`Sr.No` INT AUTO_INCREMENT PRIMARY KEY,`email` VARCHAR(50),`password` VARCHAR(50),`Message` VARCHAR(100), `Date` TEXT);";
+    $create="CREATE TABLE IF NOT EXISTS `portfolio_userData`(`Sr.No` INT AUTO_INCREMENT PRIMARY KEY,`email` VARCHAR(50),`Message` VARCHAR(100), `Date` TEXT);";
 
     mysqli_query($conn,$create);
 
-    $insert="INSERT INTO `portfolio` (`Email`,`Password`,`Message`,`Date`) VALUES ('$email','$pass','$msg','$date');";
+    $insert="INSERT INTO `portfolio_userData` (`Email`,`Message`,`Date`) VALUES ('$email','$msg','$date');";
+    // $insert="INSERT INTO `portfolio` (`Email`,`Password`,`Message`,`Date`) VALUES ('$email','$pass','$msg','$date');";
 
     if(mysqli_query($conn,$insert))
     {
-        echo "Values inserted<br>";
+        $file = file_get_contents('index.html');
+        echo $file;
+        // echo "Values inserted<br>";
     }
 ?>
